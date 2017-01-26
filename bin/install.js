8b5df787501a6baedc5ce1988f065ec54979b0e0
Darwin Gautalius <darwin.gautalius@gdplabs.id>
2017-01-26 09:53:47 +0700

    use symlink directly

--- bin/install.js
+++ bin/install.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
-const execSync = require('child_process').execSync;
+const fs = require('fs');
+const path = require('path');
 
 var package = require(process.cwd() + '/package.json');
 let packages = Object.keys(package.localDependencies);
@@ -9,8 +10,28 @@ for (var i = 0; i < packages.length; i++) {
     link(packages[i], package.localDependencies[packages[i]]);
 }
 
-function link(name, path) {
+function link(name, target) {
     console.log('linking ' + name);
-    execSync('yarn link', { cwd: path });
-    execSync('yarn link ' + name, { cwd: process.cwd() });
+    target = path.resolve(process.cwd(), target);
+    let link = path.resolve(process.cwd(), './node_modules/' + name);
+
+    prepareDirectory(link);
+    createSymlink(target, link);
+}
+
+function createSymlink(target, link) {
+    if (fs.existsSync(link)) {
+        fs.unlinkSync(link);
+    }
+    fs.symlinkSync(target, link, 'dir');
+}
+
+function prepareDirectory(path) {
+    let dirsToCreate = path.split('/');
+    dirsToCreate.pop();
+    path = dirsToCreate.join('/');
+
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
 }
